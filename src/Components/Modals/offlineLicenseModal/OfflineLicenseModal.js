d637382b0deec4058236cacff44c573015a83026
Naeem Ahmad <naeemahmad2290@gmail.com>
2023-12-13 20:14:55 +0500

    Changed the welcome note section on the landing page position from center to right side  Added login form on the same right position  Added navigations buttons in the at the same page for different components  Added animations for the component change  Validated the login form covering the edge cases  Added error messages for respective input field  Implemented the show and hide password feature and synchronized it to the password input field

--- src/Components/Modals/offlineLicenseModal/OfflineLicenseModal.js
+++ src/Components/Modals/offlineLicenseModal/OfflineLicenseModal.js
@@ -1,9 +1,8 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./OfflineLicensestyle.css";
-import VncLogo from "../../../Vnc.png"
+import VncLogo from "../../../Vnc.png";
 const OfflineLicenseModal = ({ show, setShow }) => {
   return (
     <>
@@ -25,33 +24,41 @@ const OfflineLicenseModal = ({ show, setShow }) => {
         <div className="content-container">
           <div className="offline-subheading">
             <p>
-             To license VNC Viewer offline, donwload the offline license file found on the Deployment page of your RealVNC account and apply it below.
+              To license VNC Viewer offline, donwload the offline license file
+              found on the Deployment page of your RealVNC account and apply it
+              below.
             </p>
           </div>
           <div>
             <div className="fields-wrapper">
               <label>Choose offline license file:</label>
               <div>
-                <input type="text"/>
+                <input type="text" />
                 <Button className="cancel-button">Browse</Button>
               </div>
             </div>
           </div>
 
           <div className="bottom-content">
-            <p className="bottom-headings">By licensing VNC Viewer you accept the <a href="#">T&C</a> and <a href="#">privacy policy.</a>  </p>
-            <p className="bottom-headings" >Please note that we may attempt to verify your license by contacting RealVNC services</p>
+            <p className="bottom-headings">
+              By licensing VNC Viewer you accept the <a href="#">T&C</a> and{" "}
+              <a href="#">privacy policy.</a>{" "}
+            </p>
+            <p className="bottom-headings">
+              Please note that we may attempt to verify your license by
+              contacting RealVNC services
+            </p>
           </div>
         </div>
-    
-         <hr />
-        <Modal.Footer  className='offline-modal-bottom-container'>
-            <Button className="next-button" onClick={() => setShow(false)}>
-              Next
-            </Button>
-            <Button className="cancel-button" onClick={() => setShow(false)}>
-              Cancel
-            </Button>
+
+        <hr />
+        <Modal.Footer className="offline-modal-bottom-container">
+          <Button className="next-button" onClick={() => setShow(false)}>
+            Next
+          </Button>
+          <Button className="cancel-button" onClick={() => setShow(false)}>
+            Cancel
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
