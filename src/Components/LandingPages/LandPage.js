93c9333c0e36c419605891baffac482299941cb8
Naeem Ahmad <naeemahmad2290@gmail.com>
2023-12-19 14:50:28 +0500

    Removed the outer blank box appearing in the landing page Converted the inner landing page box to the full screen

--- src/Components/LandingPages/LandPage.js
+++ src/Components/LandingPages/LandPage.js
@@ -6,29 +6,29 @@ export const LandingPage = () => {
   const [connectClicked, setConnect] = useState(false);
 
   return (
-    <div className="outerContainer">
-      <div className="innerContainer">
-        <div className="innerContainerItem">
-          <div className="mainSection">
-            {!connectClicked ? (
-              <div className="welcomeContent">
-                <h1 className="heading">Welcome</h1>
-                <h3 className="subHeading">To Our Company</h3>
-                <button
-                  onClick={() => {
-                    setConnect(!connectClicked);
-                  }}
-                  className="connectBtn"
-                >
-                  Connect
-                </button>
-              </div>
-            ) : (
-              <LoginForm setConnect={setConnect} connect={connectClicked} />
-            )}
-          </div>
+    // <div className="outerContainer">
+    <div className="innerContainer">
+      <div className="innerContainerItem">
+        <div className="mainSection">
+          {!connectClicked ? (
+            <div className="welcomeContent">
+              <h1 className="heading">Welcome</h1>
+              <h3 className="subHeading">To Our Company</h3>
+              <button
+                onClick={() => {
+                  setConnect(!connectClicked);
+                }}
+                className="connectBtn"
+              >
+                Connect
+              </button>
+            </div>
+          ) : (
+            <LoginForm setConnect={setConnect} connect={connectClicked} />
+          )}
         </div>
       </div>
     </div>
+    // </div>
   );
 };
